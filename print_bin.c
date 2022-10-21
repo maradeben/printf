1de7deb2d76237296c9241790c74bf6b66d059f6
Eben <maradebenezer@gmail.com>
2022-10-21 15:04:52 +0100

    Fixed reversed result in print binary function

--- print_bin.c
+++ print_bin.c
@@ -3,19 +3,28 @@
 /**
  * print_bin - print the binary format of number passed
  * @arg_list: argument list
- * Return: the character to print
+ * Return: the length of binary
 */
 int print_bin(va_list arg_list)
 {
 	int dec = va_arg(arg_list, int);
-	int length = 0, bin;
+	int i = 0, length = 0, bin;
+	int result[300];
+
 
 	while (dec > 0)
 	{
 		bin = dec % 2;
 		dec = dec / 2;
-		_putchar('0' + bin);
+		result[i++] = bin;
 		length += 1;
+	}
+
+	i -= 1;
+	while (i >= 0)
+	{
+		_putchar(result[i] + '0');
+		i--;
 	}
 	return (length);
 }
